217a0fa40b53f966fa454680fa9285a3c5b239d6
Humakt83 <urlact@yahoo.com>
2017-07-12 23:30:09 +0300

    Base files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojuhang
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojuhang/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojuhang.core-test
+  (:require [clojure.test :refer :all]
+            [clojuhang.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
